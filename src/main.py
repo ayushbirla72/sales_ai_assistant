ed4f2f707fcd040ab0e0fd2a61d407da3a90e62d
ayushbirla72 <dev.boxofic@gmail.com>
2025-06-02 12:05:10 +0530

    update the meeting path

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-# from src.routes.meeting import router as meeting_router
+from src.routes.meeting import router as meeting_router
 from src.routes.auth import router as auth_router
 from src.routes.suggestion import router as suggestion_router
 # from src.routes.chatBot import router as chatbot
@@ -7,7 +7,7 @@ from src.routes.calendar import router as calendar_router
 
 app = FastAPI(title="Audio Uploader with Transcription & Diarization")
 
-# app.include_router(meeting_router, prefix="/api/meeting")
+app.include_router(meeting_router, prefix="/api/meet")
 app.include_router(auth_router, prefix="/api/auth", tags=["Auth"])
 app.include_router(suggestion_router, prefix="/api/sg")
 # app.include_router(chatbot, prefix="/api/chat")
